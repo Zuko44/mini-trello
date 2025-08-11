a4501b8517364dd416e321b441dc77bca4fc0594
Zuko44 <mateo.damiani@gmail.com>
2025-08-11 14:50:11 +0300

    deleted router

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { createPersistedState } from 'pinia-plugin-persistence'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
-import router from './router'
 import './styles/base.css'
 
 const pinia = createPinia()
@@ -11,7 +10,4 @@ pinia.use(createPersistedState())
 
 const app = createApp(App)
 
-app.use(pinia)
-app.use(router)
-
-app.mount('#app')
+app.use(pinia).mount('#app')
